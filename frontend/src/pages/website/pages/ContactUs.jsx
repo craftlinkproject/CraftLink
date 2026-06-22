3b200555fb6dc04233b2a6af27a19e51760f3bfb
craftlinkproject <craftlink.project2026@gmail.com>
2026-06-22 08:39:47 +0300

    update craftlink

--- frontend/src/pages/website/pages/ContactUs.jsx
+++ frontend/src/pages/website/pages/ContactUs.jsx
@@ -15,6 +15,7 @@ import {
   Footer,
 } from "@components";
 import { useTranslation } from "react-i18next";
+import { WorldMap } from "@/components/ui/map";
 const faqData = [
   {
     question: "What is CraftLink?",
@@ -313,10 +314,34 @@ export default function ContactUs() {
         {/* MAP */}
 
         <section className="map">
-          <iframe
-            title="map"
-            src="https://maps.google.com/maps?q=mansoura&t=&z=13&ie=UTF8&iwloc=&output=embed"
-          ></iframe>
+          <WorldMap
+            dots={[
+              {
+                start: { lat: 64.2008, lng: -149.4937, label: "Fairbanks" },
+                end: { lat: 34.0522, lng: -118.2437, label: "Los Angeles" },
+              },
+              {
+                start: { lat: 64.2008, lng: -149.4937, label: "Fairbanks" },
+                end: { lat: -15.7975, lng: -47.8919, label: "Brasília" },
+              },
+              {
+                start: { lat: -15.7975, lng: -47.8919, label: "Brasília" },
+                end: { lat: 38.7223, lng: -9.1393, label: "Lisbon" },
+              },
+              {
+                start: { lat: 51.5074, lng: -0.1278, label: "London" },
+                end: { lat: 28.6139, lng: 77.209, label: "New Delhi" },
+              },
+              {
+                start: { lat: 28.6139, lng: 77.209, label: "New Delhi" },
+                end: { lat: 43.1332, lng: 131.9113, label: "Vladivostok" },
+              },
+              {
+                start: { lat: 28.6139, lng: 77.209, label: "New Delhi" },
+                end: { lat: -1.2921, lng: 36.8219, label: "Nairobi" },
+              },
+            ]}
+          />
         </section>
 
         {/* CTA */}
